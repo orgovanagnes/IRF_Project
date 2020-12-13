1bfb3eb6750ff51dd57757be09c06a2ed39c381e
orgovanagnes <58262835+orgovanagnes@users.noreply.github.com>
2020-12-13 16:04:25 +0100

    Évek szerinti adatok kiirása txt fájlba

--- IRF Projekt 1/IRF Projekt 1/Form4.Designer.cs
+++ IRF Projekt 1/IRF Projekt 1/Form4.Designer.cs
@@ -28,27 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(138, 55);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(403, 293);
-            this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(589, 90);
+            this.button1.Location = new System.Drawing.Point(244, 123);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(214, 122);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Output kiirás";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form4
             // 
@@ -56,10 +47,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
             this.Name = "Form4";
             this.Text = "Form4";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -67,6 +56,5 @@
         #endregion
 
         public System.Windows.Forms.Button button1;
-        public System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- IRF Projekt 1/IRF Projekt 1/Form4.cs
+++ IRF Projekt 1/IRF Projekt 1/Form4.cs
@@ -14,46 +14,6 @@ namespace IRF_Projekt_1
 {
     public partial class Form4 : Form
     {
-        List<Személy> Személyek = new List<Személy>();
-        List<AlkoholistaVSZ> AlkoholistaVSZs = new List<AlkoholistaVSZ>();
-        List<NemAlkoholistaVSZ> NemAlkoholistaVSZs = new List<NemAlkoholistaVSZ>();
-
-        Random rng = new Random(1234);
-        public Form4()
-        {
-            InitializeComponent();
-
-            Személyek = GetSzemélyek(@"C:\Temp\Résztvevők.csv");
-            AlkoholistaVSZs = GetAlkoholistaVSZs(@"C:\Temp\Alkoholista.csv");
-            NemAlkoholistaVSZs = GetNemAlkoholistaVSZs(@"C:\Temp\Nem_Alkoholista.csv");
-
-            // dataGridView1.DataSource = AlkoholistaVSZs;
-        }
-
-        private void Szimuláció()
-        {
-            for (int year = 2005; year <= 2024; year++)
-            {
-                
-                for (int i = 0; i < Személyek.Count; i++)
-                {
-                    
-                }
-
-                int férfiakszáma = (from x in Személyek
-                                  where x.Nem == Nem.Male && x.Alkoholista
-                                  select x).Count();
-                int nőkszáma = (from x in Személyek
-                                    where x.Nem == Nem.Female && x.Alkoholista
-                                    select x).Count();
-                Console.WriteLine(
-                    string.Format("Év:{0} Férfiak:{1} Nők:{2}", year, férfiakszáma, nőkszáma));
-
-            }
-
-        }
-
-
         public List<Személy> GetSzemélyek(string csvpath)
         {
             List<Személy> Személyek = new List<Személy>();
@@ -74,6 +34,7 @@ namespace IRF_Projekt_1
 
             return Személyek;
         }
+
 
         public List<AlkoholistaVSZ> GetAlkoholistaVSZs(string csvpath)
         {
@@ -96,7 +57,7 @@ namespace IRF_Projekt_1
             return AlkoholistaVSZs;
         }
 
-        public List<NemAlkoholistaVSZ> GetNemAlkoholistaVSZs(string csvpath)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+        public List<NemAlkoholistaVSZ> GetNemAlkoholistaVSZs(string csvpath)
         {
             List<NemAlkoholistaVSZ> NemAlkoholistaVSZs = new List<NemAlkoholistaVSZ>();
 
@@ -117,5 +78,112 @@ namespace IRF_Projekt_1
             return NemAlkoholistaVSZs;
         }
 
+        List<Személy> Személyek = new List<Személy>();
+        List<AlkoholistaVSZ> AlkoholistaVSZs = new List<AlkoholistaVSZ>();
+        List<NemAlkoholistaVSZ> NemAlkoholistaVSZs = new List<NemAlkoholistaVSZ>();
+
+
+
+        public Form4()
+        {
+            InitializeComponent();
+
+            Személyek = GetSzemélyek(@"C:\Temp\Résztvevők.csv");
+            AlkoholistaVSZs = GetAlkoholistaVSZs(@"C:\Temp\Alkoholista.csv");
+            NemAlkoholistaVSZs = GetNemAlkoholistaVSZs(@"C:\Temp\Nem_Alkoholista.csv");
+
+            // dataGridView1.DataSource = AlkoholistaVSZs;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rng = new Random();
+            int minYear = 99999;
+            int maxYear = 1;
+
+            int minAlkoholistaÉv = 9999;
+            int maxAlkholistaÉV = 1;
+
+            // csak a férfiakat tekintve a szélső értékek (legkisebb / legnagyobb évszám --- hány éves alkholisták - min/max)
+            for (int i = 0; i < Személyek.Count; i++)
+            {
+                Személy személy = Személyek[i];
+                if (személy.Nem == Nem.Female) continue;
+
+                if (személy.SzületésiÉv > maxYear)
+                {
+                    maxYear = személy.SzületésiÉv;
+                }
+
+                if (személy.SzületésiÉv < minYear)
+                {
+                    minYear = személy.SzületésiÉv;
+                }
+
+                if (személy.AlkoholistaÉv > maxAlkholistaÉV)
+                {
+                    maxAlkholistaÉV = személy.AlkoholistaÉv;
+                }
+
+                if (személy.AlkoholistaÉv < minAlkoholistaÉv)
+                {
+                    minAlkoholistaÉv = személy.AlkoholistaÉv;
+                }
+            }
+
+
+            for (int year = minYear; year < maxYear; year++)
+            {
+                /*
+                    for (int i = 0; i < Személyek.Count; i++)
+                    {
+                        Személy személy = Személyek[i];
+                        if (!személy.Alkoholista) continue;
+
+                        byte kor = (byte)(year - személy.SzületésiÉv);
+
+                        double pnemalkoholista = (from x in NemAlkoholistaVSZs
+                                               where x.Gender == személy.Nem && x.Kor == kor + 1
+                                               select x.Valószínűség).First();
+                        if (rng.NextDouble() <= pnemalkoholista) személy.Alkoholista = false;
+
+
+                        double palkoholista = (from x in AlkoholistaVSZs
+                                               where x.Kor == kor
+                                               select x.Valószínűség).FirstOrDefault();
+
+                        if (rng.NextDouble() <= palkoholista)
+                        {
+                        
+                        }
+
+
+
+                    }
+                    */
+                //            }
+
+                //}
+
+               
+                int hanyEveVagyokAlkoholista = rng.Next(minAlkoholistaÉv, maxAlkholistaÉV);
+                int numberOfEmber = (from x in Személyek
+                                     where x.SzületésiÉv == year && x.Nem == Nem.Male && x.AlkoholistaÉv == hanyEveVagyokAlkoholista
+                                     select x).Count();
+                /*int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();*/
+                //Console.WriteLine(
+                //    string.Format("Év:{0} Fiuk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+
+                using (System.IO.StreamWriter file =
+                    new System.IO.StreamWriter(@"C:\Temp\output.txt", true))
+                {
+                    file.WriteLine(string.Format("Év:{0} // Hány éve alkholista: {1} // Db ember:{2}", year, hanyEveVagyokAlkoholista, numberOfEmber));
+                }
+            }
+        }
     }
 }
